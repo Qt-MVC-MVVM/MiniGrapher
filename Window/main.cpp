edc1e4677476b89298cd9c535e85ff24f779aee7
CcCody <1250043992@qq.com>
2018-07-14 19:06:10 +0800

    Try static compiling

--- Window/main.cpp
+++ Window/main.cpp
@@ -1,3 +1,8 @@
+#ifdef QT_PLUGIN_STATIC_LINK
+#include<QtCore/QtPlugin>
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+Q_IMPORT_PLUGIN(QICOPlugin);
+#endif
 #include "mainwindow.h"
 #include <QApplication>
 
