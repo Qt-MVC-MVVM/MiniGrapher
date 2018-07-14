2791215b522f6fddcdd135f4634fc1e7370c34dd
CcCody <1250043992@qq.com>
2018-07-14 19:22:36 +0800

    Change methods to achieve static compiling

--- Window/main.cpp
+++ Window/main.cpp
@@ -1,10 +1,7 @@
-#ifdef QT_PLUGIN_STATIC_LINK
-#include<QtCore/QtPlugin>
-Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
-Q_IMPORT_PLUGIN(QICOPlugin);
-#endif
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtCore/QtPlugin>
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
 
 int main(int argc, char *argv[])
 {
