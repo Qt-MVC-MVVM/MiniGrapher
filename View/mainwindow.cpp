0e9f8fb1c2a13f6d40754249c4e68f47f95078c4
CrownX <abtx4869@163.com>
2018-07-26 03:10:56 +0800

    Add View Report

--- View/mainwindow.cpp
+++ View/mainwindow.cpp
@@ -16,7 +16,7 @@ MainWindow::MainWindow(QWidget *parent) :
     msgLabel = new QLabel;
 
     ui->CustomPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectAxes |
-                                                                    QCP::iSelectLegend | QCP::iSelectPlottables);
+                                    QCP::iSelectLegend | QCP::iSelectPlottables);
     ui->CustomPlot->xAxis->setRange(-8, 8);
     ui->CustomPlot->yAxis->setRange(-5, 5);
     ui->CustomPlot->axisRect()->setupFullAxesBox();
@@ -76,17 +76,17 @@ MainWindow::~MainWindow()
 
 void MainWindow::updateGraphSettings()
 {
-        if (!m_graph)
-                return;
+    if (!m_graph)
+        return;
 
-        ui->m_pen->setText(PenTool::name(m_graph->pen()));
-        ui->m_brush->setText(BrushTool::name(m_graph->brush()));
+    ui->m_pen->setText(PenTool::name(m_graph->pen()));
+    ui->m_brush->setText(BrushTool::name(m_graph->brush()));
 
-        m_graph->setPen(m_penTool->pen());
-        m_graph->setBrush(m_brushTool->brush());
-        m_graph->setName(ui->m_graphName->text());
+    m_graph->setPen(m_penTool->pen());
+    m_graph->setBrush(m_brushTool->brush());
+    m_graph->setName(ui->m_graphName->text());
 
-        ui->CustomPlot->replot();
+    ui->CustomPlot->replot();
 }
 
 void MainWindow::titleDoubleClick(QMouseEvent* event)
@@ -94,14 +94,14 @@ void MainWindow::titleDoubleClick(QMouseEvent* event)
     Q_UNUSED(event)
     if (QCPTextElement *title = qobject_cast<QCPTextElement*>(sender()))
     {
-        // Set the plot title by double clicking on it
-        bool ok;
-        QString newTitle = QInputDialog::getText(this, "Graphs", "New plot title:", QLineEdit::Normal, title->text(), &ok);
-        if (ok)
-        {
-            title->setText(newTitle);
-            ui->CustomPlot->replot();
-        }
+    // Set the plot title by double clicking on it
+    bool ok;
+    QString newTitle = QInputDialog::getText(this, "Graphs", "New plot title:", QLineEdit::Normal, title->text(), &ok);
+    if (ok)
+    {
+        title->setText(newTitle);
+        ui->CustomPlot->replot();
+    }
     }
 }
 
@@ -110,13 +110,13 @@ void MainWindow::axisLabelDoubleClick(QCPAxis *axis, QCPAxis::SelectablePart par
     // Set an axis label by double clicking on it
     if (part == QCPAxis::spAxisLabel) // only react when the actual axis label is clicked, not tick label or axis backbone
     {
-        bool ok;
-        QString newLabel = QInputDialog::getText(this, "Graphs", "New axis label:", QLineEdit::Normal, axis->label(), &ok);
-        if (ok)
-        {
-            axis->setLabel(newLabel);
-            ui->CustomPlot->replot();
-        }
+    bool ok;
+    QString newLabel = QInputDialog::getText(this, "Graphs", "New axis label:", QLineEdit::Normal, axis->label(), &ok);
+    if (ok)
+    {
+        axis->setLabel(newLabel);
+        ui->CustomPlot->replot();
+    }
     }
 }
 
@@ -126,14 +126,14 @@ void MainWindow::legendDoubleClick(QCPLegend *legend, QCPAbstractLegendItem *ite
     Q_UNUSED(legend)
     if (item) // only react if item was clicked (user could have clicked on border padding of legend where there is no item, then item is 0)
     {
-        QCPPlottableLegendItem *plItem = qobject_cast<QCPPlottableLegendItem*>(item);
-        bool ok;
-        QString newName = QInputDialog::getText(this, "Graphs", "New graph name:", QLineEdit::Normal, plItem->plottable()->name(), &ok);
-        if (ok)
-        {
-            plItem->plottable()->setName(newName);
-            ui->CustomPlot->replot();
-        }
+    QCPPlottableLegendItem *plItem = qobject_cast<QCPPlottableLegendItem*>(item);
+    bool ok;
+    QString newName = QInputDialog::getText(this, "Graphs", "New graph name:", QLineEdit::Normal, plItem->plottable()->name(), &ok);
+    if (ok)
+    {
+        plItem->plottable()->setName(newName);
+        ui->CustomPlot->replot();
+    }
     }
 }
 
@@ -141,29 +141,29 @@ void MainWindow::selectionChanged()
 {
     // make top and bottom axes be selected synchronously, and handle axis and tick labels as one selectable object:
     if (ui->CustomPlot->xAxis->selectedParts().testFlag(QCPAxis::spAxis) || ui->CustomPlot->xAxis->selectedParts().testFlag(QCPAxis::spTickLabels) ||
-            ui->CustomPlot->xAxis2->selectedParts().testFlag(QCPAxis::spAxis) || ui->CustomPlot->xAxis2->selectedParts().testFlag(QCPAxis::spTickLabels))
+        ui->CustomPlot->xAxis2->selectedParts().testFlag(QCPAxis::spAxis) || ui->CustomPlot->xAxis2->selectedParts().testFlag(QCPAxis::spTickLabels))
     {
-        ui->CustomPlot->xAxis2->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels);
-        ui->CustomPlot->xAxis->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels);
+    ui->CustomPlot->xAxis2->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels);
+    ui->CustomPlot->xAxis->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels);
     }
     // make left and right axes be selected synchronously, and handle axis and tick labels as one selectable object:
     if (ui->CustomPlot->yAxis->selectedParts().testFlag(QCPAxis::spAxis) || ui->CustomPlot->yAxis->selectedParts().testFlag(QCPAxis::spTickLabels) ||
-            ui->CustomPlot->yAxis2->selectedParts().testFlag(QCPAxis::spAxis) || ui->CustomPlot->yAxis2->selectedParts().testFlag(QCPAxis::spTickLabels))
+        ui->CustomPlot->yAxis2->selectedParts().testFlag(QCPAxis::spAxis) || ui->CustomPlot->yAxis2->selectedParts().testFlag(QCPAxis::spTickLabels))
     {
-        ui->CustomPlot->yAxis2->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels);
-        ui->CustomPlot->yAxis->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels);
+    ui->CustomPlot->yAxis2->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels);
+    ui->CustomPlot->yAxis->setSelectedParts(QCPAxis::spAxis|QCPAxis::spTickLabels);
     }
 
     // synchronize selection of graphs with selection of corresponding legend items:
     for (int i=0; i<ui->CustomPlot->graphCount(); ++i)
     {
-        QCPGraph *graph = ui->CustomPlot->graph(i);
-        QCPPlottableLegendItem *item = ui->CustomPlot->legend->itemWithPlottable(graph);
-        if (item->selected() || graph->selected())
-        {
-            item->setSelected(true);
-            graph->setSelection(QCPDataSelection(graph->data()->dataRange()));
-        }
+    QCPGraph *graph = ui->CustomPlot->graph(i);
+    QCPPlottableLegendItem *item = ui->CustomPlot->legend->itemWithPlottable(graph);
+    if (item->selected() || graph->selected())
+    {
+        item->setSelected(true);
+        graph->setSelection(QCPDataSelection(graph->data()->dataRange()));
+    }
     }
 }
 
@@ -173,11 +173,11 @@ void MainWindow::mousePress()
     // if no axis is selected, both directions may be dragged
 
     if (ui->CustomPlot->xAxis->selectedParts().testFlag(QCPAxis::spAxis))
-        ui->CustomPlot->axisRect()->setRangeDrag(ui->CustomPlot->xAxis->orientation());
+    ui->CustomPlot->axisRect()->setRangeDrag(ui->CustomPlot->xAxis->orientation());
     else if (ui->CustomPlot->yAxis->selectedParts().testFlag(QCPAxis::spAxis))
-        ui->CustomPlot->axisRect()->setRangeDrag(ui->CustomPlot->yAxis->orientation());
+    ui->CustomPlot->axisRect()->setRangeDrag(ui->CustomPlot->yAxis->orientation());
     else
-        ui->CustomPlot->axisRect()->setRangeDrag(Qt::Horizontal|Qt::Vertical);
+    ui->CustomPlot->axisRect()->setRangeDrag(Qt::Horizontal|Qt::Vertical);
 }
 
 void MainWindow::mouseWheel()
@@ -186,11 +186,11 @@ void MainWindow::mouseWheel()
     // if no axis is selected, both directions may be zoomed
 
     if (ui->CustomPlot->xAxis->selectedParts().testFlag(QCPAxis::spAxis))
-        ui->CustomPlot->axisRect()->setRangeZoom(ui->CustomPlot->xAxis->orientation());
+    ui->CustomPlot->axisRect()->setRangeZoom(ui->CustomPlot->xAxis->orientation());
     else if (ui->CustomPlot->yAxis->selectedParts().testFlag(QCPAxis::spAxis))
-        ui->CustomPlot->axisRect()->setRangeZoom(ui->CustomPlot->yAxis->orientation());
+    ui->CustomPlot->axisRect()->setRangeZoom(ui->CustomPlot->yAxis->orientation());
     else
-        ui->CustomPlot->axisRect()->setRangeZoom(Qt::Horizontal|Qt::Vertical);
+    ui->CustomPlot->axisRect()->setRangeZoom(Qt::Horizontal|Qt::Vertical);
 }
 
 void MainWindow::addRandomGraph()
@@ -207,8 +207,8 @@ void MainWindow::addRandomGraph()
     QVector<double> x(n), y(n);
     for (int i=0; i<n; i++)
     {
-        x[i] = (i/(double)n-0.5)*10.0*xScale + xOffset;
-        y[i] = (qSin(x[i]*r1*5)*qSin(qCos(x[i]*r2)*r4*3)+r3*qCos(qSin(x[i])*r4*2))*yScale + yOffset;
+    x[i] = (i/(double)n-0.5)*10.0*xScale + xOffset;
+    y[i] = (qSin(x[i]*r1*5)*qSin(qCos(x[i]*r2)*r4*3)+r3*qCos(qSin(x[i])*r4*2))*yScale + yOffset;
     }
 
     ui->CustomPlot->addGraph();
@@ -216,75 +216,75 @@ void MainWindow::addRandomGraph()
     ui->CustomPlot->graph()->setData(x, y);
     ui->CustomPlot->graph()->setLineStyle((QCPGraph::LineStyle)(rand()%5+1));
     if (rand()%100 > 50)
-        ui->CustomPlot->graph()->setScatterStyle(QCPScatterStyle((QCPScatterStyle::ScatterShape)(rand()%14+1)));
+    ui->CustomPlot->graph()->setScatterStyle(QCPScatterStyle((QCPScatterStyle::ScatterShape)(rand()%14+1)));
     QPen graphPen;
     graphPen.setColor(QColor(rand()%245+10, rand()%245+10, rand()%245+10));
     graphPen.setWidthF(rand()/(double)RAND_MAX*2+1);
     ui->CustomPlot->graph()->setPen(graphPen);
 
     if (ui->CustomPlot->graphCount() > 0)
-                ui->CustomPlot->legend->setVisible(true);
+        ui->CustomPlot->legend->setVisible(true);
 
     ui->CustomPlot->replot();
 }
 
 void MainWindow::plotGraph()
 {
-        ui->CustomPlot->addGraph();
-        ui->CustomPlot->graph()->setName(ui->Function->text());
-        ui->CustomPlot->xAxis2->setVisible(true);
-        ui->CustomPlot->xAxis2->setTickLabels(false);
-        ui->CustomPlot->yAxis2->setVisible(true);
-        ui->CustomPlot->yAxis2->setTickLabels(false);
+    ui->CustomPlot->addGraph();
+    ui->CustomPlot->graph()->setName(ui->Function->text());
+    ui->CustomPlot->xAxis2->setVisible(true);
+    ui->CustomPlot->xAxis2->setTickLabels(false);
+    ui->CustomPlot->yAxis2->setVisible(true);
+    ui->CustomPlot->yAxis2->setTickLabels(false);
 
-        // make left and bottom axes always transfer their ranges to right and top axes:
-        // pass data points to graphs:
-        ui->CustomPlot->graph()->setData(*x, *y);
-        // let the ranges scale themselves so graph 0 fits perfectly in the visible area:
-        ui->CustomPlot->graph()->rescaleAxes();
+    // make left and bottom axes always transfer their ranges to right and top axes:
+    // pass data points to graphs:
+    ui->CustomPlot->graph()->setData(*x, *y);
+    // let the ranges scale themselves so graph 0 fits perfectly in the visible area:
+    ui->CustomPlot->graph()->rescaleAxes();
 
-        ui->CustomPlot->graph()->setPen(QPen(QColor(0, 94, 161, 255)));
-        ui->CustomPlot->graph()->setBrush(QBrush(QColor(156, 212, 255, 40)));
-        if (ui->CustomPlot->graphCount() > 0)
-                    ui->CustomPlot->legend->setVisible(true);
+    ui->CustomPlot->graph()->setPen(QPen(QColor(0, 94, 161, 255)));
+    ui->CustomPlot->graph()->setBrush(QBrush(QColor(156, 212, 255, 40)));
+    if (ui->CustomPlot->graphCount() > 0)
+        ui->CustomPlot->legend->setVisible(true);
 
-        ui->Function->clear();
+    ui->Function->clear();
 
-        ui->CustomPlot->replot();
+    ui->CustomPlot->replot();
 }
 
 void MainWindow::setIntegral(std::shared_ptr<double> IntegralAns)
 {
-        this->IntegralAns = IntegralAns;
+    this->IntegralAns = IntegralAns;
 }
 
 void MainWindow::setDifferential(std::shared_ptr<double> DifferentialAns)
 {
-        this->DifferentialAns = DifferentialAns;
+    this->DifferentialAns = DifferentialAns;
 }
 
 void MainWindow::showDifferential()
 {
-        ui->Differential->setText(QString::number(*DifferentialAns));
+    ui->Differential->setText(QString::number(*DifferentialAns));
 }
 
 void MainWindow::showIntegral()
 {
-        ui->Integral->setText(QString::number(*IntegralAns));
+    ui->Integral->setText(QString::number(*IntegralAns));
 }
 
 void MainWindow::showPaintFailed()
 {
-        QString failure = "Wrong input.";
-        msgLabel->setText(failure);
-        ui->statusBar->addWidget(msgLabel);
+    QString failure = "Wrong input.";
+    msgLabel->setText(failure);
+    ui->statusBar->addWidget(msgLabel);
 }
 
 void MainWindow::showPaintSucceed()
 {
-        QString success = "Plot graph succeeded.";
-        msgLabel->setText(success);
-        ui->statusBar->addWidget(msgLabel);
+    QString success = "Plot graph succeeded.";
+    msgLabel->setText(success);
+    ui->statusBar->addWidget(msgLabel);
 }
 
 void MainWindow::removeSelectedGraph()
@@ -292,17 +292,17 @@ void MainWindow::removeSelectedGraph()
     if (ui->CustomPlot->selectedGraphs().size() > 0)
     {
 
-        ui->CustomPlot->graph()->setPen(QPen(QColor(0, 94, 161, 255)));
-        ui->CustomPlot->graph()->setBrush(QBrush(QColor(156, 212, 255, 40)));
-        m_graph = 0;
+    ui->CustomPlot->graph()->setPen(QPen(QColor(0, 94, 161, 255)));
+    ui->CustomPlot->graph()->setBrush(QBrush(QColor(156, 212, 255, 40)));
+    m_graph = 0;
 
-        ui->CustomPlot->removeGraph(ui->CustomPlot->selectedGraphs().first());
+    ui->CustomPlot->removeGraph(ui->CustomPlot->selectedGraphs().first());
 
-        ui->m_graphName->clear();
-        if (ui->CustomPlot->graphCount() <= 0)
-                    ui->CustomPlot->legend->setVisible(false);
+    ui->m_graphName->clear();
+    if (ui->CustomPlot->graphCount() <= 0)
+            ui->CustomPlot->legend->setVisible(false);
 
-        ui->CustomPlot->replot();
+    ui->CustomPlot->replot();
     }
 }
 
@@ -316,7 +316,7 @@ void MainWindow::removeAllGraphs()
 
     ui->m_graphName->clear();
     if (ui->CustomPlot->graphCount() <= 0)
-                ui->CustomPlot->legend->setVisible(false);
+        ui->CustomPlot->legend->setVisible(false);
 
     ui->CustomPlot->replot();
 }
@@ -328,18 +328,18 @@ void MainWindow::contextMenuRequest(QPoint pos)
 
     if (ui->CustomPlot->legend->selectTest(pos, false) >= 0) // context menu on legend requested
     {
-        menu->addAction("Move to top left", this, SLOT(moveLegend()))->setData((int)(Qt::AlignTop|Qt::AlignLeft));
-        menu->addAction("Move to top center", this, SLOT(moveLegend()))->setData((int)(Qt::AlignTop|Qt::AlignHCenter));
-        menu->addAction("Move to top right", this, SLOT(moveLegend()))->setData((int)(Qt::AlignTop|Qt::AlignRight));
-        menu->addAction("Move to bottom right", this, SLOT(moveLegend()))->setData((int)(Qt::AlignBottom|Qt::AlignRight));
-        menu->addAction("Move to bottom left", this, SLOT(moveLegend()))->setData((int)(Qt::AlignBottom|Qt::AlignLeft));
+    menu->addAction("Move to top left", this, SLOT(moveLegend()))->setData((int)(Qt::AlignTop|Qt::AlignLeft));
+    menu->addAction("Move to top center", this, SLOT(moveLegend()))->setData((int)(Qt::AlignTop|Qt::AlignHCenter));
+    menu->addAction("Move to top right", this, SLOT(moveLegend()))->setData((int)(Qt::AlignTop|Qt::AlignRight));
+    menu->addAction("Move to bottom right", this, SLOT(moveLegend()))->setData((int)(Qt::AlignBottom|Qt::AlignRight));
+    menu->addAction("Move to bottom left", this, SLOT(moveLegend()))->setData((int)(Qt::AlignBottom|Qt::AlignLeft));
     } else    // general context menu on graphs requested
     {
-        menu->addAction("Add random graph", this, SLOT(addRandomGraph()));
-        if (ui->CustomPlot->selectedGraphs().size() > 0)
-            menu->addAction("Remove selected graph", this, SLOT(removeSelectedGraph()));
-        if (ui->CustomPlot->graphCount() > 0)
-            menu->addAction("Remove all graphs", this, SLOT(removeAllGraphs()));
+    menu->addAction("Add random graph", this, SLOT(addRandomGraph()));
+    if (ui->CustomPlot->selectedGraphs().size() > 0)
+        menu->addAction("Remove selected graph", this, SLOT(removeSelectedGraph()));
+    if (ui->CustomPlot->graphCount() > 0)
+        menu->addAction("Remove all graphs", this, SLOT(removeAllGraphs()));
     }
 
     menu->popup(ui->CustomPlot->mapToGlobal(pos));
@@ -349,13 +349,13 @@ void MainWindow::moveLegend()
 {
     if (QAction* contextAction = qobject_cast<QAction*>(sender())) // make sure this slot is really called by a context menu action, so it carries the data we need
     {
-        bool ok;
-        int dataInt = contextAction->data().toInt(&ok);
-        if (ok)
-        {
-            ui->CustomPlot->axisRect()->insetLayout()->setInsetAlignment(0, (Qt::Alignment)dataInt);
-            ui->CustomPlot->replot();
-        }
+    bool ok;
+    int dataInt = contextAction->data().toInt(&ok);
+    if (ok)
+    {
+        ui->CustomPlot->axisRect()->insetLayout()->setInsetAlignment(0, (Qt::Alignment)dataInt);
+        ui->CustomPlot->replot();
+    }
     }
 }
 
@@ -382,17 +382,17 @@ void MainWindow::graphClicked(QCPAbstractPlottable *plottable, int dataIndex)
 
 void MainWindow::set_paint_command(std::shared_ptr<ICommandBase> ptrCommand)
 {
-        PaintCommand = ptrCommand;
+    PaintCommand = ptrCommand;
 }
 
 void MainWindow::set_differential_command(std::shared_ptr<ICommandBase> ptrCommand)
 {
-        DifferentialCommand = ptrCommand;
+    DifferentialCommand = ptrCommand;
 }
 
 void MainWindow::set_integral_command(std::shared_ptr<ICommandBase> ptrCommand)
 {
-        IntegralCommand = ptrCommand;
+    IntegralCommand = ptrCommand;
 }
 
 std::shared_ptr<IPropertyNotification> MainWindow::getPtrWindowProSink()
@@ -407,42 +407,41 @@ std::shared_ptr<ICommandNotification> MainWindow::getPtrWindowSetSink()
 
 void MainWindow::on_AddGraphButton_clicked()
 {
+    bool ok(false);
+    QString qstr = ui->Function->text();
+    QString qlower = ui->LowerBound->text();
+    QString qupper = ui->UpperBound->text();
+    std::string str = qstr.toStdString();
 
-        bool ok(false);
-        QString qstr = ui->Function->text();
-        QString qlower = ui->LowerBound->text();
-        QString qupper = ui->UpperBound->text();
-        std::string str = qstr.toStdString();
+    double lower = qlower.isEmpty()? -10: qlower.toDouble(&ok);
+    double upper = qupper.isEmpty()?  10: qupper.toDouble(&ok);
 
-        double lower = qlower.isEmpty()? -10: qlower.toDouble(&ok);
-        double upper = qupper.isEmpty()?  10: qupper.toDouble(&ok);
+    PaintCommand->SetParameter(str, lower, upper);
+    PaintCommand->Exec();
 
-        PaintCommand->SetParameter(str, lower, upper);
-        PaintCommand->Exec();
+    DifferentialCommand->SetParameter(str, lower);
+    DifferentialCommand->Exec();
 
-        DifferentialCommand->SetParameter(str, lower);
-        DifferentialCommand->Exec();
-
-        IntegralCommand->SetParameter(str, lower, upper);
-        IntegralCommand->Exec();
+    IntegralCommand->SetParameter(str, lower, upper);
+    IntegralCommand->Exec();
 }
 
 void MainWindow::on_RemoveGraphButton_clicked()
 {
-        removeSelectedGraph();
+    removeSelectedGraph();
 }
 
 void MainWindow::on_ClearAllButton_clicked()
 {
-        removeAllGraphs();
+    removeAllGraphs();
 }
 
 void MainWindow::set_x(std::shared_ptr<QVector<double> > x)
 {
-        this->x = x;
+    this->x = x;
 }
 
 void MainWindow::set_y(std::shared_ptr<QVector<double> > y)
 {
-        this->y = y;
+    this->y = y;
 }
